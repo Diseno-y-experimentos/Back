499b2babf40d3713e38b627e33449b5796df8679
JoaCUPE <u20201f788@upc.edu.pe>
2026-05-12 20:49:00 -0500

    feat: updat back

--- BusTrackBackEnd/BusTrackBackEnd.API/obj/Debug/net9.0/BusTrackBackEnd.API.AssemblyInfo.cs
+++ BusTrackBackEnd/BusTrackBackEnd.API/obj/Debug/net9.0/BusTrackBackEnd.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusTrackBackEnd.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77f104b514fa0b24c51dd1aad49585177ceed20b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c487b5389f07bfe797aab8729b0816718a4175")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusTrackBackEnd.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusTrackBackEnd.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
